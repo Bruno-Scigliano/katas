ad46f7f1540cee59440cac2c9f4a3def27653419
Bruno Scigliano <bruno1457@gmail.com>
2014-09-05 06:21:12 -0300

    Modificada funcion Tartamudeo usando iterate sobre basicamente lo mismo

--- code-katas-2/src/code_katas_2/core.clj
+++ code-katas-2/src/code_katas_2/core.clj
@@ -44,20 +44,16 @@
 
 
 (defn tartamudeo
-  "Escriba una funcion que retorne una secuencia lazy que comprima el tartamudeo de una secuencia de numeros.
-   Comprimir el tartamudeo se refiere a que [1 1 1] se exprese como [3 1] y a su vez [3 1] se exprese como [1 3 1 1].
-
-   La funcion debe aceptar una secuencia inicial de numeros, y devolver una secuencia infinita de compresiones, donde
-   cada nuevo elemento es el elemento anterior comprimido."
+  "Escriba una funcion que tome un predicado de 2 argumentos, un valor y una coleccion, y
+   retorne una nueva coleccion donde el valor es insertado intercalado cada dos argumentos
+   que cumplan el predicado"
   [s]
-	  (defn j [sec cont ant res] 
-		(if (empty? sec)
-			(into res [cont ant])
-			(if (= (first sec) ant)
-				(recur (rest sec) (inc cont) (first sec) res)
-				(recur (rest sec) 1 (first sec) (into res [cont ant]))
-				)
-			)
-		)
-	  (lazy-seq (cons (j (rest s) 1 (first s) [] ) (tartamudeo (j (rest s) 1 (first s) []))))
-	  )
+  (rest (iterate 
+      (fn [a] ((fn [sec cont ant res] (if (empty? sec) (concat res [cont] [ant])
+                                        (if (= (first sec) ant)
+                                          (recur (rest sec) (inc cont) (first sec) res)
+                                          (recur (rest sec) 1 (first sec) (into res [cont ant])))))
+                (rest a) 1 (first a) [])
+        )s)
+    )
+  )
